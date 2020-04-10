2464fb4b6212cdf0dddbbf427ad82129978f7af4
Sidhartha <kondetisidhartha@gmail.com>
2020-04-10 22:20:50 +0200

    Added Semantic UI

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Revents App!!</h1>
+        <button className='ui icon button'>
+          <i className='world icon'></i>
+          CSS Button
+        </button>
+
+        {/* Button from React component */}
+        <Button
       </div>
     );
   }
