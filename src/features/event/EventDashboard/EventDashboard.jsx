ac0de726821f8b57449838044809bc94abd29dc8
Sidhartha <kondetisidhartha@gmail.com>
2020-04-11 02:27:02 +0200

    Finished EventList Column

--- src/features/event/EventDashboard/EventDashboard.jsx
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { Grid } from 'semantic-ui-react';
+import EventList from '../EventList/EventList';
 
 class EventDashboard extends Component {
   render() {
     return (
       <div>
         <Grid>
-          <Grid.Column width={10}>Left Column</Grid.Column>
+          <Grid.Column width={10}>
+            <EventList />
+          </Grid.Column>
           <Grid.Column width={6}>Right Column</Grid.Column>
         </Grid>
       </div>
